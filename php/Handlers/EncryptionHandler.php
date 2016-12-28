7684aeb47b7c69682871bb520712f8b176487d57
Eric Mann <eric@eamann.com>
2016-12-27 18:58:10 -0800

    Switch to using a more secure crypto library
    The original encryption middleware copy-pasted the encryption setup from
    the PHP manual as an attempt to a) show how the manual could be used and
    b) point out that the code in the manual could/should be improved.
    Unfortunately, some people didn't understand that ...
    
    Switch out the homegrown implementation for the PHP Encryption library
    from Taylor Hornby.
    
    Fixes #1

--- php/Handlers/EncryptionHandler.php
+++ php/Handlers/EncryptionHandler.php
@@ -13,6 +13,8 @@
 
 namespace EAMann\Sessionz\Handlers;
 
+use Defuse\Crypto\Crypto;
+
 /**
  * Extend the standard "noop" handler so that normal functionality for managing
  * sessions passes through unchanged to other layers in the stack. However, be sure
@@ -28,70 +30,30 @@ class EncryptionHandler extends NoopHandler  {
         $this->key = $key;
     }
 
-    /**
-     * Attempt to decrypt the ciphertext passed in given the key supplied by the
-     * constructor.
-     *
-     * Code derived from examples available in the PHP documentation
-     * @see http://php.net/manual/en/class.sessionhandler.php
-     *
-     * @param string $ciphertext
-     *
-     * @return string
-     */
-    protected function decrypt( $ciphertext )
+	/**
+	 * Attempt to decrypt the ciphertext passed in given the key supplied by the
+	 * constructor.
+	 *
+	 * @param string $ciphertext
+	 *
+	 * @return string
+	 */
+    protected function decrypt($ciphertext)
     {
-        $data = base64_decode($ciphertext);
-        $salt = substr($data, 0, 16);
-        $encrypted = substr($data, 16);
-
-        // Regenerate the key and initialization vector given available data
-        $rounds = 3;
-        $data00 = $this->key . $salt;
-        $hash = array();
-        $hash[0] = hash('sha256', $data00, true);
-        $result = $hash[0];
-        for ($i = 1; $i < $rounds; $i++) {
-            $hash[$i] = hash('sha256', $hash[$i - 1].$data00, true);
-            $result .= $hash[$i];
-        }
-        $key = substr($result, 0, 32);
-        $iv  = substr($result, 32,16);
-
-        return openssl_decrypt($encrypted, 'AES-256-CBC', $key, true, $iv);
+	    return Crypto::decrypt($ciphertext, $this->key);
     }
 
-    /**
-     * Encrypt the plain text and return a ciphertext encrypted with the key supplied
-     * by the constructor.
-     *
-     * Code derived from examples available in the PHP documentation
-     * @see http://php.net/manual/en/class.sessionhandler.php
-     *
-     * @param string $plaintext
-     *
-     * @return string
-     */
-    protected function encrypt( $plaintext )
+	/**
+	 * Use the Crypto object from Defuse's library to encrypt the plain text with the key
+	 * supplied in the constructor.
+	 *
+	 * @param string $plaintext
+	 *
+	 * @return string
+	 */
+    protected function encrypt($plaintext)
     {
-        // Generate a random salt to be used in encryption
-        $salt = openssl_random_pseudo_bytes(16);
-
-        // Generate a uniform key and initialization vector from the supplied key
-        $salted = '';
-        $dx = '';
-        while (strlen($salted) < 48) {
-            $dx = hash('sha256', $dx . $this->key . $salt, true);
-            $salted .= $dx;
-        }
-
-        $key = substr($salted, 0, 32);
-        $iv  = substr($salted, 32,16);
-
-        // Do the actual encryption
-        $encrypted_data = openssl_encrypt($plaintext, 'AES-256-CBC', $key, true, $iv);
-
-        return base64_encode($salt . $encrypted_data);
+	    return Crypto::encrypt($plaintext, $this->key);
     }
 
     /**
